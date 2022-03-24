dd4f181483335491370aa657f70261e0abc05097
RUDRA DEV VERMA <72748856+rdv18@users.noreply.github.com>
2022-03-24 17:49:30 +0530

    Update main.go

--- CRUD for Contact/main.go
+++ CRUD for Contact/main.go
@@ -10,11 +10,11 @@ import (
 func initializeRouter() {
 	r := mux.NewRouter()
 
-	r.HandleFunc("/users", GetUsers).Methods("GET")
-	r.HandleFunc("/users/{id}", GetUser).Methods("GET")
-	r.HandleFunc("/users", CreateUser).Methods("POST")
-	r.HandleFunc("/users/{id}", UpdateUser).Methods("PUT")
-	r.HandleFunc("/users/{id}", DeleteUser).Methods("DELETE")
+	r.HandleFunc("/users", GetContacts).Methods("GET")
+	r.HandleFunc("/users/{id}", GetContact).Methods("GET")
+	r.HandleFunc("/users", CreateContact).Methods("POST")
+	r.HandleFunc("/users/{id}", UpdateContact).Methods("PUT")
+	r.HandleFunc("/users/{id}", DeleteContact).Methods("DELETE")
 
 	log.Fatal(http.ListenAndServe(":9000", r))
 }
